2932d3dde65cb19575d3c5a18fddb8d49c2a588a
Young Soul Business <nlvanasperen@hotmail.com>
2026-05-12 14:20:53 +0200

    Temporarily disable update checker to isolate activation fatal

--- hipsy-events.php
+++ hipsy-events.php
@@ -10,7 +10,7 @@
  * Plugin Name:   Hipsy Events Core
  * Plugin URI:    https://www.youngsoulbusiness.com
  * Description:   Core plugin voor Hipsy Events. Verzorgt API-koppeling, event sync, custom post type, instellingen en builder-onafhankelijke event data voor WordPress.
- * Version:       4.6.6
+ * Version:       4.6.7
  * Author:        Young Soul Business & How About Yes
  * Author URI:    https://www.youngsoulbusiness.com
  * Text Domain:   hipsy-events
@@ -19,7 +19,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 if ( ! defined( 'HIPSY_EVENTS_CORE_VERSION' ) ) {
-    define( 'HIPSY_EVENTS_CORE_VERSION', '4.6.6' );
+    define( 'HIPSY_EVENTS_CORE_VERSION', '4.6.7' );
 }
 
 if ( ! defined( 'HIPSY_EVENTS_CORE_PATH' ) ) {
@@ -30,22 +30,8 @@ if ( ! defined( 'HIPSY_EVENTS_CORE_URL' ) ) {
     define( 'HIPSY_EVENTS_CORE_URL', plugin_dir_url( __FILE__ ) );
 }
 
-// Plugin Update Checker - Core updates via GitHub releases.
-$hipsy_puc_file = plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php';
-
-if ( file_exists( $hipsy_puc_file ) ) {
-    require_once $hipsy_puc_file;
-
-    if ( class_exists( '\\YahnisElsts\\PluginUpdateChecker\\v5\\PucFactory' ) ) {
-        $hipsy_updater = \\YahnisElsts\\PluginUpdateChecker\\v5\\PucFactory::buildUpdateChecker(
-            'https://github.com/Nickvanasperen/hipsy-events-core/',
-            __FILE__,
-            'hipsy-events-core'
-        );
-
-        $hipsy_updater->getVcsApi()->enableReleaseAssets();
-    }
-}
+// Plugin Update Checker tijdelijk uitgeschakeld om activatie-fatal uit te sluiten.
+// Zodra Core stabiel activeert, zetten we deze gecontroleerd terug.
 
 // Core includes gebaseerd op de originele stabiele Hipsy Events plugin.
 if ( file_exists( __DIR__ . "/templates/loopItem.php" ) ) include(__DIR__ . "/templates/loopItem.php");
